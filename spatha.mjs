b64e5f18618abae2377c6223dc3108c64c84abb2
danielmascena <danielmascena@gmail.com>
2018-12-07 12:06:50 -0500

    fixing html broken code

--- spatha.mjs
+++ spatha.mjs
@@ -4,6 +4,8 @@
  @author: danielmascena
 ************************************************************************************/
 
+'use strict';
+
 const innerHTML = Symbol('innerHTML');
 
 function htmlEscape(str) { return str.replace(/&/g, '&amp;').replace(/>/g, '&gt;').replace(/</g, '&lt;').replace(/"/g, '&quot;').replace(/'/g, '&#39;').replace(/`/g, '&#96;'); }
