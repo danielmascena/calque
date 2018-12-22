b74522d6cf1a4b6ca11f1e2b589cac7ef973c154
danmascena.droid <danielmascena@gmail.com>
2018-12-22 19:26:54 -0300

    refactoring

---
+++ index.mjs
@@ -0,0 +1,22 @@
+    'use strict';
+
+    import { innerHTML, html } from './shaula.mjs';
+   import ScorpiusElement from './scorpius.mjs';
+    
+    export function main(){
+    
+    const sco = new ScorpiusElement;
+        sco.setAttribute('name', 'Sco');
+        document.body.appendChild(sco);
+        
+        
+    let p = document.createElement('p');
+p[innerHTML] = html`
+  Lorem, ipsum dolor sit amet consectetur adipisicing elit. <span style="${{"color": "green"}}">
+  Tincidunt</span> ornare massa eget egestas purus. Bibendum enim facilisis gravida neque convallis a. 
+  Vitae suscipit tellus mauris a diam maecenas. Ultricies leo integer malesuada nunc vel risus commodo 
+  viverra maecenas.
+`;
+document.body.appendChild(p);
+
+}
---
+++ scorpius.mjs
@@ -0,0 +1,22 @@
+    
+    import { innerHTML, html } from './shaula.mjs';
+    
+    export class ScorpiusElement extends HTMLElement {
+          static get observedAttributes() { return ['name']; }
+          constructor(...args) {
+            super(...args);
+            this.stars = ['Antares', 'Lesath', 'Graffias'];
+          }
+          attributeChangedCallback() { this.render(); }
+          connectedCallback() { this.render(); }
+          render() {
+            this[innerHTML] = html`
+              <p id onblur='${(e)=>console.log(e.target.textContent)}' class='par' contenteditable>&#955; ♏ (see browser console)</p>
+              <h1 onclick='${function(){alert('ok')}}' style="${{"color": "red", "font-size": "5em"}}">Hello, &lambda; ${this.getAttribute('name')}</h1>
+              <ul>
+              ${this.stars.map(name => html`<li onclick='${ev=>alert(name)}'>${name}</li>`)}
+              </ul>
+            `;
+          }
+        }
+        customElements.define('sco-const', ScorpiusElement);
