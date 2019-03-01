022540a7fff804c6061809d5d6a5a71ef43037af
Daniel Mascena <danielmascena@gmail.com>
2019-02-28 21:05:39 -0300

    adding babel

---
+++ test/main.mjs
@@ -0,0 +1,26 @@
+/***********************************************************************************
+    Example Code
+    ************************************************************************************/
+
+'use strict';
+
+import {innerHTML, html} from '../src/engraft.mjs';
+import GenericElement from './generic.mjs';
+
+export default function() {
+
+  const sco = new GenericElement;
+  sco.setAttribute('name', 'Sco');
+  document.body.appendChild(sco);
+
+  let p = document.createElement('p');
+  p[innerHTML] = html `
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      <span style="${ {"color" : "green"} }">
+      Tincidunt</span> ornare massa eget egestas purus. Bibendum enim facilisis gravida
+      neque convallis a. Vitae suscipit tellus mauris a diam maecenas. Ultricies leo
+      integer malesuada nunc vel risus commodo viverra maecenas.
+  `;
+
+  document.body.appendChild(p);
+}
