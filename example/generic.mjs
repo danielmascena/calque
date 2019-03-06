b6ae613aac8bc700be165ff02799094ba505c7d3
danmascena.droid <danielmascena@gmail.com>
2019-03-05 22:00:02 -0300

    using es5 approach for (possible) shadow html error

--- example/generic.mjs
+++ example/generic.mjs
@@ -18,7 +18,7 @@ export default class GenericElement extends HTMLElement {
   connectedCallback() {
     this.render();
   }
-  _clickHandler(){
+  _clickHandler: function clickHandler(){
     alert(this.num);
   }
   _changeHandler(){
